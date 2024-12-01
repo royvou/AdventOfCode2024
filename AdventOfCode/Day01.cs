a027648b33dcd36554cb3302cb852c84d1fc7420
Roy <royvou@hotmail.com>
2024-12-01 17:28:59 +0100

    chore: Day1 - Optimize

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -11,23 +11,25 @@ public class Day01 : BaseDay
 
     public override ValueTask<string> Solve_1() => new(CalculatePart1(_input).ToString());
 
-    private int CalculatePart1(string input)
+    private static long CalculatePart1(string input)
     {
-        var b = input.Split(Environment.NewLine).Select(x => x.Split("   ")).ToArray();
-        var column1 = b.Select(x => x[0]).Select(int.Parse).OrderBy(x => x);
-        var column2 = b.Select(x => x[1]).Select(int.Parse).OrderBy(x => x);
+        var parsedInput = ParseInput(input);
+        var column1 = parsedInput.Select(x => x.One).OrderBy(x => x);
+        var column2 = parsedInput.Select(x => x.Two).OrderBy(x => x);
 
         return column1.Zip(column2).Select(x => Math.Abs(x.First - x.Second)).Sum();
     }
 
+    private static ( long One, long Two)[] ParseInput(string input)
+        => input.Split(Environment.NewLine).Select(x => x.Split("   ")).Select(x => (One: x[0].AsLong(), Two: x[1].AsLong())).ToArray();
+
     public override ValueTask<string> Solve_2() => new(CalculatePart2(_input).ToString());
 
-    private int CalculatePart2(string input)
+    private static long CalculatePart2(string input)
     {
-        var b = input.Split(Environment.NewLine).Select(x => x.Split("   ")).ToArray();
-        var column1 = b.Select(x => x[0]).Select(int.Parse).OrderBy(x => x);
-        var column2 = b.Select(x => x[1]).Select(int.Parse);
+        var parsedInput = ParseInput(input);
+        var column2 = parsedInput.Select(x => x.Two).CountBy(x => x).ToDictionary(x => x.Key, y => y.Value);
 
-        return column1.Zip(column2).Select(x => x.First * column2.Count(y => y == x.First)).Sum();
+        return parsedInput.Select(x => x.One * column2.GetValueOrDefault(x.One, 0)).Sum();
     }
 }
---
+++ AdventOfCode/Extensions.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode;
+
+public static class Extensions
+{
+    public static long AsLong(this string input) => long.Parse(input);
+
+    public static IEnumerable<long> AsLongArray(this IEnumerable<string> input) => input.Select(AsLong);
+}
